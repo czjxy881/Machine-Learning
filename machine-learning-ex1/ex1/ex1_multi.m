168384a88d1f4bb6c681732f807cd6638598888e
善仁 <xinyu.jxy@alipay.com>
2017-05-02 11:01:31 +0800

    完成ex1

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -88,6 +88,7 @@ num_iters = 400;
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
 [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
+display(theta);
 
 % Plot the convergence graph
 figure;
@@ -95,16 +96,36 @@ plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
 xlabel('Number of iterations');
 ylabel('Cost J');
 
+
+hold on;
+alpha = 0.1;
+theta1 = zeros(3, 1);
+[theta1, J_history1] = gradientDescentMulti(X, y, theta1, alpha, num_iters);
+plot(1:numel(J_history1), J_history1, '-g', 'LineWidth', 2);
+display(theta1);
+hold on;
+alpha = 0.003;
+theta2 = zeros(3, 1);
+[theta2, J_history2] = gradientDescentMulti(X, y, theta2, alpha, num_iters);
+plot(1:numel(J_history2), J_history2, '-r', 'LineWidth', 2);
+display(theta2);
 % Display gradient descent's result
 fprintf('Theta computed from gradient descent: \n');
-fprintf(' %f \n', theta);
+fprintf(' %f \n', theta1);
 fprintf('\n');
+
+
+
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
 price = 0; % You should change this
+customFeature=[1650 3];
+customFeature=[1 (customFeature-mu)./sigma];
+display(customFeature);
+price=customFeature*theta1;
 
 
 % ============================================================
@@ -150,7 +171,7 @@ fprintf('\n');
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
 price = 0; % You should change this
-
+price=[1 1650 3]*theta;
 
 % ============================================================
 
